6fbf74db35bb43a9582050f29390abe73d211b3d
_Srinivas_ <glsrinivas1819@gmail.com>
2026-01-28 14:32:14 +0530

    m

--- src/main/java/com/example/CareerGuaidance/controller/AuthController.java
+++ src/main/java/com/example/CareerGuaidance/controller/AuthController.java
@@ -4,15 +4,15 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.CareerGuaidance.dto.LoginDto;
 import com.example.CareerGuaidance.dto.Registrationdto;
 import com.example.CareerGuaidance.service.AuthService;
 
-@Controller
+@RestController
 public class AuthController {
 	
 	private AuthService service;
