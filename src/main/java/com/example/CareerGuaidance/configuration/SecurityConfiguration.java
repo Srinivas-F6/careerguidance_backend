b680e8c04d6cc56af12a78e9e15b27f9e1ff62fe
_Srinivas_ <glsrinivas1819@gmail.com>
2026-01-28 11:32:32 +0530

    cors

--- src/main/java/com/example/CareerGuaidance/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/CareerGuaidance/configuration/SecurityConfiguration.java
@@ -79,7 +79,11 @@ public class SecurityConfiguration {
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration config = new CorsConfiguration();
 
-	    config.setAllowedOrigins(List.of("http://localhost:5173"));
+	    config.setAllowedOrigins(List.of(
+	            "http://localhost:5173",
+	            "https://careerguidance-frontend.vercel.app"
+	        ));
+	    
 	    config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 	    config.setAllowedHeaders(List.of("Authorization","Content-Type"));
 	    config.setAllowCredentials(true);
